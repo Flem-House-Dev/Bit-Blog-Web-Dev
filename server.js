3354b598e04f400968024d7f0f4730e809200479
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-06-22 10:51:04 -0500

    added server.js, helper.js, and connection.js files

---
+++ server.js
@@ -0,0 +1,47 @@
+// imported node modules
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// In-app imports
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+require('dotenv').config();
+
+// Server initialization
+const app = express();
+const PORT = process.env.DB_URL || 3001;
+
+// Session configuration
+const sess = {
+    secret: SESS_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+// Handlebars initialization
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Middle-ware
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+app.use(routes);
+
+// Static page
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Servier listener
+sequelize.sync({ force:false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on PORT: ${PORT}`));
+});
