c24347e36fafcfbe406e2bb17f34956ecb10f2da
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-06-22 12:23:07 -0500

    added db models

---
+++ models/Blogs.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const { blockParams } = require("handlebars");
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blog",
+  }
+);
+
+module.exports = Blog;
