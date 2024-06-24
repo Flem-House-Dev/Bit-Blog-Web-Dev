1ef6b34169248f3bf96dd13094406949d3b17da4
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-06-24 08:25:10 -0500

    added Foundation css

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
+const { Blog } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
-        res.render('homepage')
+
+        const dbBlogData = await Blog.findAll();
+        const blogs = dbBlogData.map((blog) =>
+            blog.get({ plain: true})
+        );
+
+        res.render('homepage', {
+            blogs
+        });
+
     } catch (err) {
         console.error(err);
         res.status(500).json(err);
