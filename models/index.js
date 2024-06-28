bc5750def2089c6b91b1ef7a984d76989a5d7534
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-06-28 14:04:25 -0500

    added comment model and updated model index

--- models/index.js
+++ models/index.js
@@ -1,13 +1,35 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
+const { blockParams } = require('handlebars');
 
 User.hasMany(Blog, {
     foreignKey: 'id',
     onDelete: 'CASCADE'
 });
 
+User.hasMany(Comment, {
+    foreignKey: 'id',
+    onDelete: 'CASCADE'
+})
+
 Blog.belongsTo(User, {
     foreignKey: 'id'
+});
+
+Blog.hasMany(Comment, {
+    foreignKey: "id",
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "id",
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Blog, {
+    foreignKey: "id",
+    onDelete: 'CASCADE'
 });
 
 module.exports = { User, Blog };
