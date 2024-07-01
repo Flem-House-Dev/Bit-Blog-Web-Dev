8235d61e6de77cf208e2f93cd209fcf8f1f2b550
Flem-House-Dev <144638343+Flem-House-Dev@users.noreply.github.com>
2024-07-01 15:44:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const PORT = process.env.PORT || 3001;
 const sess = {
     secret: "salsa",
     cookie: {},
+    maxAge: 300000,
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
